bab688fecf1781d54419a259f4c054947cbb7e3f
solar464 <solar464@gmail.com>
2016-02-26 15:29:43 -0800

    after implementing 3 suggested changes
    Not working, Block.getInstance() appears to be malfunctioning

---
+++ src/Block.java
@@ -0,0 +1,339 @@
+import java.util.*;
+//representation of a block using it dimensions and position on a (Board)
+public class Block
+{
+   //x, y refer to coordinates of top left corner
+   private int hght, wdth;
+   private Pair pos;
+   private static HashMap<Integer, Block> allPieces;
+   
+   //debug controllers
+   private static boolean BLOCK1 = false, BLOCK2 = false, BLOCK3 = false;
+   public static boolean getBLOCK1(){ return BLOCK1; }
+   public static void setDebug(boolean one, boolean two, boolean three){
+      BLOCK1 = one; BLOCK2 = two; BLOCK3 = three;
+      if(BLOCK1){
+         setCount(0);
+         setInstanceCount(0);
+      }
+      if(BLOCK3)
+         resetTimes();
+   }
+   
+   //counts the number of (Block) objects created
+   private static int count;
+   public static void setCount(int num){ count = num; }
+   public static int getCount(){ return count; }
+   public static void printCount(){
+      if(BLOCK1)
+         System.out.println("Number of (Block) objects created: " + count); 
+   }
+   
+   //track total runtime of various methods
+   private static long startTime;
+   private static long shouldBeEmptyTime, moveTime;
+   public static void resetTimes(){ shouldBeEmptyTime = 0; moveTime = 0; }
+   public static long getShouldBeEmpty(){ return shouldBeEmptyTime; }
+   public static long getMove(){ return moveTime; }
+   public static void printTimes(){
+      if(BLOCK3){
+         System.out.println("\nTotal runtimes of relevant methods in (Block): ");
+         System.out.println("shouldBeEmpty(): " + shouldBeEmptyTime/1000000 +
+               "\nmove(): " + moveTime/1000000);
+      }   
+   }
+   
+   private static int instanceCount;
+   public static void setInstanceCount(int i){ instanceCount = i; }
+   public static int getInstanceCount(){ return instanceCount; }
+   public static void printInstanceCount(){
+      System.out.println("Number of (Block) references created: " + instanceCount);
+   }
+   
+   //initialize storage of (Block)s
+   public static void initializePieces(){
+      if(allPieces == null)
+         allPieces = new HashMap<Integer, Block>();
+   }
+   
+   //retrieve (Block) with the desired information, create it if it doesn't exist
+   public static Block getInstance(int height, int width, int xPos, int yPos){
+      instanceCount++;
+      int hash = height*10^3 + width*10^2 + yPos*10 + xPos;
+      Block toReturn = allPieces.get(hash);
+      if(toReturn == null){
+         toReturn = new Block(height, width, xPos, yPos);
+         allPieces.put(hash, toReturn);
+      }
+      System.out.println(height+ ""+width+yPos+xPos + ":" + toReturn);
+      return toReturn;
+   }
+   public static Block getInstance(int height, int width, Pair pos){
+      return getInstance(height, width, pos.getX(), pos.getY());
+   }
+   
+   //constructor
+   private Block(int height, int width, int xPos, int yPos){
+      hght = height;
+      wdth = width;
+      pos = Pair.getInstance(xPos, yPos);
+      count++;
+   }
+   
+   private Block(int height, int width, Pair coor){
+      hght = height;
+      wdth = width;
+      pos = coor;
+      count++;
+   }
+   //various accessor methods
+   public int getHeight(){ return hght; }
+   public int getWidth() { return wdth; }
+   public Pair getPos()  { return pos; }
+   public int getXPos()  { return pos.getX(); }
+   public int getYPos()  { return pos.getY(); }
+   
+   /*
+   //two (Block)s only equal if all their instance data is equal
+   public boolean equals(Object other){
+      if(!(other instanceof Block))
+         return false;
+      return ((Block)other).pos == pos && ((Block)other).hght == hght && ((Block)other).wdth == wdth;
+   }
+   */
+   
+   //for use in storing in (Board)s
+   public int hashCode(){
+      return (hght + "" + wdth + pos.toString()).hashCode();
+   }
+   
+   //calculates the empty spaces required for (this) to move in the given (direction)
+   //returns null if cannot move in the given direction
+   public LinkedList<Pair> shouldBeEmpty(Pair direction, HashSet<Pair> empty){
+      startTime = System.nanoTime();
+      int numToCheck;
+      Pair beginCheck, dirToCheck;
+      if(direction == Pair.UP){
+         numToCheck = wdth;
+         beginCheck = pos.add(Pair.UP);
+         dirToCheck = Pair.RIGHT;
+      }
+      else if(direction == Pair.DOWN){
+         numToCheck = wdth;
+         beginCheck = pos.add(Pair.DOWN.mult(hght));
+         dirToCheck = Pair.RIGHT;
+      }
+      else if(direction == Pair.RIGHT){
+         numToCheck = hght;
+         beginCheck = pos.add(Pair.RIGHT.mult(wdth));
+         dirToCheck = Pair.DOWN;
+      }
+      else if(direction == Pair.LEFT){
+         numToCheck = hght;
+         beginCheck = pos.add(Pair.LEFT);
+         dirToCheck = Pair.DOWN;
+      }
+      else{
+         if(BLOCK3)
+            shouldBeEmptyTime += System.nanoTime() - startTime;
+         return null;
+      }
+      
+      //(beginCheck is null if the blocks is trying to move out of the board
+      if(beginCheck == null || !empty.contains(beginCheck)){
+         if(BLOCK3)
+            shouldBeEmptyTime += System.nanoTime() - startTime;
+         return null;
+      }
+      
+      //begin generating list to return, null if needed space not there
+      LinkedList<Pair> toCheck = new LinkedList<Pair>();
+      toCheck.add(beginCheck);
+      Pair maybeEmpty;
+      for(int i = 1; i < numToCheck; i++){
+         maybeEmpty = beginCheck.add(dirToCheck.mult(i));
+         if(!empty.contains(maybeEmpty)){
+            if(BLOCK3)
+               shouldBeEmptyTime += System.nanoTime() - startTime;
+            return null;
+         }
+         toCheck.add(maybeEmpty); 
+      }
+      
+      if(BLOCK3)
+         shouldBeEmptyTime += System.nanoTime() - startTime;
+      return toCheck;
+      
+   }
+   
+   /*
+   //No longer used, function absorbed by shouldBeEmpty()
+   //checks if the spaces required for movement are actually empty
+   //(should) should be the Pair[] generated from shouldBeEmpty()
+   //(is) should be (Board.getSpaces()), the (Board)'s empty spaces
+   public boolean canMove(Pair[] should, HashSet<Pair> is){
+      startTime = System.nanoTime();
+      if(should == null || should.length == 0){
+         if(BLOCK3)
+            canMoveTime += System.nanoTime() - startTime;
+         return false;
+      }
+      for(Pair space: should)
+         if(!is.contains(space)){
+            if(BLOCK3)
+               canMoveTime += System.nanoTime() - startTime;
+            return false;
+         }
+      
+      if(BLOCK3)
+         canMoveTime += System.nanoTime() - startTime;
+      return true;
+   }
+   */
+   
+   //returns a (Block) as if moved (this) in the given (direction) 
+   //updates (spaces) according to (toFill), the spaces to be occupied, and the 
+   //  spaces to be vacated
+   public Block move(Pair direction, LinkedList<Pair> toFill, HashSet<Pair> spaces){
+      if(BLOCK2){
+         System.out.println(this + " attempting to move in direction " + direction);
+         //System.out.println("Current empty spaces: " + spaces);
+      }
+      startTime = System.nanoTime();
+      Pair oppSide;
+      int numToOpp;
+      if(toFill.size() == wdth)
+         numToOpp = hght;
+      else
+         numToOpp = wdth;
+      
+      for(Pair p: toFill){
+         spaces.remove(p);
+         
+         if(direction == Pair.UP || direction == Pair.LEFT)
+            oppSide = p.add(direction.mult(-numToOpp));
+
+         else //if(direction == Pair.DOWN || direction == Pair.RIGHT)
+            oppSide = p.sub(direction.mult(numToOpp));
+         
+         spaces.add(oppSide);
+      }      
+      Block toReturn = getInstance(hght,wdth,pos.add(direction));
+      
+      if(BLOCK3)
+         moveTime += System.nanoTime() - startTime;
+      if(BLOCK2)
+         //System.out.println("New (Block) data: " + this + "\tEmpty spaces: " + spaces);
+         System.out.println("New (Block) data: " + toReturn + "\tEmpty spaces: ");
+      
+      return toReturn;
+   }
+   
+   /*
+   //no longer used
+   //checks if a given coordinate lies within the area of (this)
+   public boolean withinBlock(Pair coor){
+      return coor.getX() >= getXPos() && coor.getX() < getXPos() + wdth &&
+             coor.getY() >= getYPos() && coor.getY() < getYPos() + hght;
+   }
+   */
+   
+   public String toString(){
+      return hght + " " + wdth + " " + pos;
+   }
+   
+   /*
+   //for use in move generation, (Block) movements in one (Board) should not
+   //  affect the (Block)s of other (Board)s
+   public Block deepClone(){
+      return new Block(hght, wdth, pos);
+   }
+   */
+   
+   //various tests, testing initialization and movement
+   public static void main(String[] args){
+      Pair.generateTable(4, 5);
+      initializePieces();
+      Block b = getInstance(2,2,1,1);
+      HashSet<Pair> space = new HashSet<Pair>();
+      space.add(Pair.getInstance(0,1));
+      space.add(Pair.getInstance(0,2));
+      space.add(Pair.getInstance(1,0));
+      space.add(Pair.getInstance(2,0));
+      
+      System.out.println("move() testing: ");
+      LinkedList<Pair> into;
+      for(Pair toMove: Pair.DIRS){
+         into = b.shouldBeEmpty(toMove, space);
+         System.out.println("\nCurrent empty spaces: " + space);
+         System.out.println("Block's information: " + b);
+         System.out.println("Direction to move: " + toMove);
+         System.out.println("Required empty spaces: " + into);
+      
+         if(into != null){
+            System.out.println("Able to move.");
+            b = b.move(toMove, into, space);
+         }
+         else
+            System.out.println("No movement possible");
+      
+      
+         System.out.println("Block's new information: " + b);
+         into = b.shouldBeEmpty(toMove, space);
+         System.out.println("Required empty spaces to move again: " + into);
+      }
+      
+      System.out.println("\nmove() testing with semi-valid input");
+      Block b1 = new Block(2, 1, 0, 0);
+      space = new HashSet<Pair>();
+      Pair toMove = Pair.getInstance(2,0);
+      try{
+         into = b1.shouldBeEmpty(toMove, space);
+         for(Pair p: into)
+            space.add(p);
+      }
+      catch(Exception e){
+         System.out.println("Unable to test invalid directions due to shouldBeEmpty()");
+         System.out.println("...Proceeding to bypass the method and generate needed space by hand");
+         space.add(Pair.getInstance(3, 4));
+         space.add(Pair.getInstance(1, 3));
+         into = new LinkedList<Pair>();
+         into.add(Pair.getInstance(3, 4));
+         into.add(Pair.getInstance(1, 3));
+      }
+      
+      System.out.println("Current empty spaces: " + space);
+      System.out.println("Block's information: " + b1);
+      System.out.println("Direction to move: " + toMove);
+      System.out.println("Required empty spaces: " + into);
+   
+      if(into != null){
+         System.out.println("Able to move.");
+         try{b1 = b1.move(toMove, into, space);}
+         catch(Exception e){System.out.println("Despite valid (into) and (space), errors occurred due to invalid (toMove)");}
+      }
+      else
+         System.out.println("No movement possible");
+   
+   
+      System.out.println("Block's new information: " + b1);
+      into = b1.shouldBeEmpty(toMove, space);
+      System.out.println("Required empty spaces to move again: " + into);
+      System.out.println("Current empty spaces: " + space);
+      
+      System.out.println("\nequals() testing");
+      Block b2 = new Block(0,0,1,1);
+      Block b3 = getInstance(0,0,1,1);
+      Block b4 = new Block(0,1,1,1);
+      Block b5 = b2;
+      System.out.println(b2 + " and " + b3 + " : " + b2.equals(b3));
+      System.out.println(b2 + " and " + b4 + " : " + b2.equals(b4));
+      System.out.println(b2 + " and " + b5 + " : " + b2.equals(b5));
+      System.out.println(b2 + " and " + 1 + " : " + b2.equals(1));
+      
+      System.out.println("\nHashCode() testing");
+      System.out.println(b2 + " hashCode = " + b2.hashCode());
+      System.out.println(b4 + " hashCode = " + b4.hashCode());
+
+   }
+}
---
+++ src/Pair.java
@@ -0,0 +1,230 @@
+import java.util.Arrays;
+import java.util.HashSet;
+
+
+public class Pair
+{
+   //for use in moving blocks
+   public final static Pair UP = new Pair(0,-1), 
+                            DOWN = new Pair(0,1),
+                            LEFT = new Pair(-1,0),
+                            RIGHT = new Pair(1,0);
+   
+   //for looping purposes in Solver.solve()
+   public final static Pair[] DIRS = { UP, LEFT, RIGHT, DOWN };
+   
+   //instance variables
+   private int x, y;
+   
+   //debugging controls
+   private static boolean PAIR1 = false, PAIR2 = false, PAIR3 = false;
+   public static void setDebug(boolean one, boolean two, boolean three){
+      PAIR1 = one; PAIR2 = two; PAIR3 = three;
+      if(PAIR1)
+         setCount(0);
+      if(PAIR2)
+         setInstanceCount(0);
+   }
+   
+   //counts the number of (Pair) objects created
+   private static int count;
+   
+   public static void setCount(int num){ count = num; }
+   public static int getCount(){ return count; }
+   public static void printCount(){
+      if(PAIR1)
+         System.out.println("Number of (Pair) objects created besides (table) and directions: " + count);
+   }
+   
+   //counts the number of references to (Pair) objects created
+   private static int instanceCount;
+   
+   public static void setInstanceCount(int num){ instanceCount = num; }
+   public static int getInstanceCount(){ return instanceCount; }
+   public static void printInstanceCount(){
+      if(PAIR2)
+         System.out.println("Number of (Pair) references created: " + instanceCount);
+   }
+   
+   //count the frequency of (Pair) use for each unique (Pair)
+   private static int[][] freq;
+   public static int[][] getFreq(){ return freq; }
+   public static void printFreq(){
+      if(PAIR3 && freq != null){
+         System.out.println("Frequency of Pair use: ");
+         for(int i = 0; i < freq[0].length; i++){
+            for(int j = 0; j < freq.length; j++){
+               System.out.print("[" + freq[j][i] + "]");
+            }
+            System.out.print("\n");
+         }
+      }
+   }
+   public static void resetFreq(int h, int w){
+      if(PAIR3)
+         freq = new int[w][h]; 
+   }
+   
+   //table to store references to all Pair objects that may be needed to avoid
+   //   wasting memory creating redundant Pair objects
+   private static Pair[][] table = null;
+   
+   //returns a reference to the Pair that contains the coordinate given
+   //returns null if beyond the dimensions of (table) and therefore, the board
+   public static Pair getInstance(int xCoor, int yCoor){
+      instanceCount++;
+      
+      try{
+         if(freq != null)
+            freq[xCoor][yCoor]++;
+         return table[xCoor][yCoor]; 
+      }
+      catch(ArrayIndexOutOfBoundsException a){ return null; }
+      catch(NullPointerException n){
+         System.out.println("Table of pairs not initialized"); 
+         return null;
+      }
+   }
+   
+   //initializes (table) with needed Pair objects, used in Solver.main
+   //will require regeneration of table if beginning new puzzle with different dimensions
+   public static void generateTable(int width, int height){
+      //prevent erasure of original table, alter if consecutive solving required
+      if(table != null)
+         return;
+               
+      table = new Pair[width][height];
+      for(int i = 0; i < width; i++)
+         for(int j = 0; j < height; j++)
+            table[i][j] = new Pair(i, j);
+   }
+   
+   //constructor only used in generateTable() and final static Pair at top
+   private Pair(int xCoor, int yCoor){
+      x = xCoor; y = yCoor;
+      count++;
+   }
+   
+   //various accessor methods
+   public int getX() { return x; }
+   public int getY() { return y; }
+   public static int getW(){return table.length;}
+   public static int getH(){return table[0].length;}
+   
+   //add operation, returns reference to the Pair that contains the sum of the
+   // two given Pairs if it exists
+   public Pair add(Pair other){
+      if(other == null)
+         return null;
+      return getInstance(x + other.x, y + other.y); 
+   }
+   
+   //Pair objects other than UP and LEFT cannot have negative numbers
+   public Pair sub(Pair other){
+      if(other == null){
+         return null;
+      }
+      return getInstance(x - other.x, y - other.y); 
+   }
+   
+   //returns reference to a multiple of the Pair
+   public Pair mult(int m){
+      return getInstance(x * m, y * m);
+   }
+   
+   //largely used for HashSet when storing empty spaces
+   public int hashCode(){
+      if(x < 0 || y < 0)
+         return (x + "" + y).hashCode();
+      return Integer.parseInt(x + "" + y);
+   }
+   
+   public String toString(){
+      return y + " " + x;
+   }
+   
+   //calculates the manhattan distance between two (Pair)s
+   public static int manhattanDis(Pair here, Pair there){
+      return Math.abs(here.x - there.x) + Math.abs(here.y - there.y);
+   }
+   
+   //various operations testing Pair operations and initialization
+   public static void main(String args[]){
+      Pair.generateTable(4, 4);
+      System.out.println("Available Pair objects after generateTable(4,4): " + Arrays.deepToString(table));
+      Pair.generateTable(0, 0);
+      System.out.println("Available Pair objects after second generateTable(0,0): " + Arrays.deepToString(table));
+      System.out.println("\nAttempting getInstance(5,0): " + getInstance(5,0));
+      System.out.println("Attempting getInstance(-3,0): " + getInstance(-3,0));
+      System.out.println("Attempting getInstance(0,0): " + getInstance(0,0));
+      System.out.println("Attempting new Pair(5,0): " + new Pair(5,0));
+      
+      
+      System.out.println("\nHashCode testing: ");
+      Pair a = getInstance(1,0);
+      System.out.println("Current Pair: " + a);
+      System.out.println("HashCode of " + a + " = " + a.hashCode());
+
+      System.out.println("\nequals() testing: ");
+      System.out.println("Different objects: " + a.equals(new Pair(1,0)));
+      System.out.println("Using getInstance() : " + a.equals(getInstance(1,0)));
+      
+      System.out.println("\nadd() testing: ");
+      Pair b = getInstance(2,3);
+      Pair c = getInstance(3,3);
+      Pair zero = new Pair(0,0);
+      System.out.println(a + " + " + b + " = " + a.add(b));
+      System.out.println(a + " + " + c + " = " + a.add(c));
+      System.out.println(a + " + " + zero + " = " + a.add(zero));
+
+      
+      System.out.println("\nsub() testing: ");
+      System.out.println(b + " - " + a + " = " + b.sub(a));
+      System.out.println(a + " - " + c + " = " + a.sub(c));
+      System.out.println(c + " - " + a + " = " + c.sub(a));
+      System.out.println(a + " - " + a + " = " + a.sub(a));
+      System.out.println(a + " - " + zero + " = " + a.sub(zero));
+
+      System.out.println("\nmult() testing");
+      Pair d = a.mult(2);
+      System.out.println(a + " * 2 = " + d);
+      System.out.println(a + " * 5 = " + a.mult(5));
+      System.out.println(a + " * 0 = " + a.mult(0));
+      System.out.println(a + " * -1 = " + a.mult(-1));
+
+      
+      System.out.println("\nHashSet testing");
+      HashSet<Pair> s = new HashSet<Pair>();
+      s.add(getInstance(0,0));s.add(getInstance(0,1));s.add(getInstance(0,2));
+      System.out.println("Original set s = " + s);
+      
+      System.out.println("s1 = s");
+      HashSet<Pair> s1 = s;
+      s1.remove(getInstance(0,0));
+      System.out.println("After removing (0,0): ");
+      System.out.println("s = " + s);
+      System.out.println("s1 = " + s1);
+      
+      s.add(getInstance(0,0));
+      System.out.println("\nOriginal set s = " + s);
+      System.out.println("After copying references into s2 and removing (0,0) from s2:");
+      
+      HashSet<Pair> s2 = new HashSet<Pair>();
+      for(Pair p: s)
+         s2.add(p);
+      
+      s2.remove(getInstance(0,0));
+      
+      System.out.println("s = " + s);
+      System.out.println("s2 = " + s2);
+      
+      System.out.println("manhattanDis() testing:");
+      System.out.println(a + " to " + c + " = " + manhattanDis(a, c));
+      System.out.println(c + " to " + a + " = " + manhattanDis(c, a));
+      System.out.println(a + " to " + a + " = " + manhattanDis(a, a));
+      System.out.println(a + " to " + zero + " = " + manhattanDis(a, zero));
+      System.out.println("(-123,456) to (0,455) = " + manhattanDis(new Pair(-123, 456), new Pair(0,455)));
+
+
+   }
+}
